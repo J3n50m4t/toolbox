6c8200828076ea20f5f4b55a7ba6e299bb081e0e
J3n50m4t <markusdittrich1@gmail.com>
2018-05-01 00:27:41 +0200

    git clone changed to ssh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ sudo apt-get install git -y
 sudo mkdir /opt/toolbox
 sudo chown 1000:1000 /opt/toolbox
 sudo chmod 750 /opt/toolbox
-git clone https://github.com/J3n50m4t/toolbox.git /opt/toolbox
+git clone git@github.com:J3n50m4t/toolbox.git /opt/toolbox
 sudo chmod 750 /opt/toolbox -R
 mkdir /opt/toolbox/userconfigs
 /opt/toolbox/toolbox
