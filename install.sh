802afec01fb8a94d5a1dca4124be7643e9f01831
J3n50m4t <J3n50m4t@users.noreply.github.com>
2018-04-30 23:45:58 +0200

    testing install

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ sudo mkdir /opt/toolbox
 sudo chown 1000:1000 /opt/toolbox
 sudo chmod 750 /opt/toolbox
 git clone https://github.com/J3n50m4t/toolbox.git /opt/toolbox
-sudo chmod 750 /opt/toolbox -R
+sudo chmod 777 /opt/toolbox -R
 /opt/toolbox/toolbox
