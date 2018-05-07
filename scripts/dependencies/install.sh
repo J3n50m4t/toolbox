b833352b61f6c57fb044ee0da558f3037d07d262
J3n50m4t <markusdittrich1@gmail.com>
2018-05-07 11:24:51 +0200

    installs reordered and progress bar adjusted

--- scripts/dependencies/install.sh
+++ scripts/dependencies/install.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-#installing
-## docker
-## ansible
-
-echo "0" | dialog --gauge "Installing docker" 10 70 0
+echo "0" | dialog --gauge "Updating repo" 10 70 0
+sudo apt-get update
+echo "10" | dialog --gauge "Installing docker" 10 70 0
 sudo apt-get install docker docker.io -y 1>/dev/null 2>&1
-echo "14" | dialog --gauge "Installing ansible" 10 70 0
+echo "20" | dialog --gauge "Installing ansible" 10 70 0
 sudo apt-get install ansible -y 1>/dev/null 2>&1
-echo "28" | dialog --gauge "Installing python" 10 70 0
-sudo apt-get install python-pip -y 1>/dev/null 2>&1
-echo "42" | dialog --gauge "Installing docker-py" 10 70 0
-sudo pip install docker-py 1>/dev/null 2>&1
-echo "50" | dialog --gauge "Installing curl unzip wget" 10 70 0
-sudo apt-get install unzip curl wget -y 1>/dev/null 2>&1
+echo "30" | dialog --gauge "Installing Unzip" 10 70 0
+sudo apt-get install unzip -y 1>/dev/null 2>&1
+echo "40" | dialog --gauge "Installing curl" 10 70 0
+sudo apt-get install curl -y 1>/dev/null 2>&1
+echo "50" | dialog --gauge "Installing wget" 10 70 0
+sudo apt-get install wget -y 1>/dev/null 2>&1
+echo "60" | dialog --gauge "Installing unionfs-fuse" 10 70 0
 sudo apt-get install unionfs-fuse -y 1>/dev/null 2>&1
-
-echo "56" |  dialog --gauge "Installing portainer" 10 70 0
+echo "70" | dialog --gauge "Installing python-pip" 10 70 0
+sudo apt-get install python-pip -y 1>/dev/null 2>&1
+echo "80" | dialog --gauge "Installing docker-py" 10 70 0
+sudo pip install docker-py 1>/dev/null 2>&1
+echo "90" | dialog --gauge "Setting up docker and installing Portainer" 10 70 0
 ansible-playbook /opt/toolbox/ansiblescripts/toolbox.yml --tags network &>/dev/null &
-echo "70" | dialog --gauge "Installing ansible" 10 70 0
 ansible-playbook /opt/toolbox/ansiblescripts/toolbox.yml --tags portainer &>/dev/null &
-echo "75" |  dialog --gauge "Installing traefik" 10 70 0
 echo "100" | dialog --gauge "Everything installed successfuly, going back to main menu" 10 70 0
 sleep 2
 sudo bash /opt/toolbox/menu.sh
