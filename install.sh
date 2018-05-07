6f2d0217be09cacb4a8940cc49ceb72e12598bbc
J3n50m4t <J3n50m4t@users.noreply.github.com>
2018-05-07 10:24:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ sudo apt-get install git -y
 sudo mkdir -p /opt/toolbox
 sudo chown 1000:1000 /opt/toolbox
 sudo chmod 750 /opt/toolbox
-sudo git clone git@github.com:J3n50m4t/toolbox.git /opt/toolbox
+git clone https://github.com/J3n50m4t/toolbox.git /opt/toolbox
 sudo chmod 750 /opt/toolbox -R
 mkdir -p /opt/toolbox/userconfigs
 sudo cp /opt/toolbox/toolbox /bin
